802ae54aca26670760d3b9bebc16d4a29850194f
Sumit Singh <sumsingh@nvidia.com>
2014-04-21 11:09:58 +0530

    smp: enhance power efficiency
    Here we are trying to reduce power usage through the
    use of macros cpu_relaxed_read_short and cpu_relaxed_read.
    
    Bug 1440421
    
    Change-Id: I114d122cf58b1cf7b93b4b5f5d712360a2f1e096
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-on: http://git-master/r/425947
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Signed-off-by: Andrei F <luxneb@gmail.com>
    
    Conflicts:
    	kernel/smp.c
    Signed-off-by: Jesse Chan <jc@linux.com>

--- kernel/smp.c
+++ kernel/smp.c
@@ -2,6 +2,7 @@
  * Generic helpers for smp ipi calls
  *
  * (C) Jens Axboe <jens.axboe@oracle.com> 2008
+ * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
  */
 #include <linux/irq_work.h>
 #include <linux/rcupdate.h>
@@ -15,6 +16,7 @@
 #include <linux/cpu.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/smp.h>
+#include <linux/sched.h>
 #include <asm/relaxed.h>
 
 #include "smpboot.h"
