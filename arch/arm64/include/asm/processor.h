10a10bb80e6ce016ceef8c601213d664d640eb5d
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2015-03-02 19:19:14 +0000

    arm64: Implement cpu_relax as yield
    ARM64 has the yield nop hint which has the intended semantics of
    cpu_relax. Implement.
    
    The immediate application is ARM CPU emulators. An emulator can take
    advantage of the yield hint to de-prioritise an emulated CPU in favor
    of other emulation tasks. QEMU A64 SMP emulation has yield awareness,
    and sees a significant boot time performance increase with this change.
    
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Jesse Chan <jc@linux.com>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -79,6 +79,7 @@ struct thread_struct {
 	unsigned long		tp_value;
 	struct fpsimd_state	fpsimd_state;
 	unsigned long		fault_address;	/* fault info */
+	unsigned long		fault_code;	/* ESR_EL1 value */
 	struct debug_info	debug;		/* debugging */
 };
 
@@ -127,7 +128,11 @@ extern void release_thread(struct task_struct *);
 
 unsigned long get_wchan(struct task_struct *p);
 
-#define cpu_relax()			barrier()
+static inline void cpu_relax(void)
+{
+	asm volatile("yield" ::: "memory");
+}
+#define cpu_relax_lowlatency()                cpu_relax()
 
 /* Thread switching */
 extern struct task_struct *cpu_switch_to(struct task_struct *prev,
