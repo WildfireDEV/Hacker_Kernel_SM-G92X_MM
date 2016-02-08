d779cca3a5cb7e4e4e1e030a034c4c63ae1b8cf3
HRTKernel <support@hacker-kernel.com>
2016-02-08 14:20:04 +0100

    add dynamic read-ahead

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1605,6 +1605,8 @@ void task_dirty_inc(struct task_struct *tsk);
 #define VM_MAX_READAHEAD	256	/* kbytes */
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
+extern unsigned long max_readahead_pages;
+
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
 
