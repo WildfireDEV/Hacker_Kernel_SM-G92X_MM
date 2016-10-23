c3b0b30d3ef62b3d2fa04c9d38ea1907951720a3
WildfireDEV <wildfiredev@gmail.com>
2016-10-24 07:59:18 +1100

    Revert "Squash of AndreiLux's power efficiency patch set"
    This reverts commit 94c34b05d4d54336148ac5f3a7cc9be91024833e.

--- arch/arm64/include/asm/relaxed.h
+++ arch/arm64/include/asm/relaxed.h
@@ -15,19 +15,6 @@
 #ifndef _ASM_RELAXED_H_
 #define _ASM_RELAXED_H_
 
-#include <asm/atomic.h>
-
-#ifndef cpu_relaxed_read_short
-#define cpu_relaxed_read_short(p)	ldax16(p)
-#endif
-
-#ifndef cpu_relaxed_read
-#define cpu_relaxed_read(p)		ldax32(p)
-#endif
-
-#ifndef cpu_relaxed_read_long
-#define cpu_relaxed_read_long(p)	ldax64((u64 *)p)
-#endif
-
 #include <asm-generic/relaxed.h>
+
 #endif /*_ASM_RELAXED_H_*/
