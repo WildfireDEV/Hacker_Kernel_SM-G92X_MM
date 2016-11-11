eed82f1368968490fbd5c89d45fe1cefb00fbe96
WildfireDEV <wildfiredev@gmail.com>
2016-11-11 18:30:24 +1100

    Revert "cpufreq: interactive: reduce timer with screen off"
    This reverts commit 6bf55c58ba33d647c19cff5e1956f8fd003d8f99.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -34,7 +34,6 @@
 #include <linux/kernel_stat.h>
 #include <linux/pm_qos.h>
 #include <linux/powersuspend.h>
-#include <linux/display_state.h>
 #include <asm/cputime.h>
 
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
@@ -91,7 +90,6 @@ static bool suspended = false;
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
-#define SCREEN_OFF_TIMER_RATE ((unsigned long)(60 * USEC_PER_MSEC))
 #define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
 static unsigned int default_above_hispeed_delay[] = {
 	DEFAULT_ABOVE_HISPEED_DELAY };
@@ -158,7 +156,6 @@ struct cpufreq_interactive_tunables {
 	 * The sample rate of the timer used to increase frequency
 	 */
 	unsigned long timer_rate;
-	unsigned long prev_timer_rate;
 	/*
 	 * Wait this long before raising speed above hispeed, by default a
 	 * single timer interval.
@@ -753,17 +750,6 @@ static void cpufreq_interactive_timer(unsigned long data)
 		wake_up_process(tunables->regionchange_task);
 	}
 #endif
-
-	if (suspended
- 		&& tunables->timer_rate != tunables->prev_timer_rate)
- 		tunables->timer_rate = tunables->prev_timer_rate;
- 	else if (!suspended
- 		&& tunables->timer_rate != SCREEN_OFF_TIMER_RATE) {
-		tunables->prev_timer_rate = tunables->timer_rate;
-		tunables->timer_rate
-			= max(tunables->timer_rate,
- 				SCREEN_OFF_TIMER_RATE);
-	}
 
 	if ((cpu_load >= tunables->go_hispeed_load || tunables->boosted) && !suspended) {
 		if (pcpu->policy->cur < tunables->hispeed_freq) {
@@ -1485,7 +1471,6 @@ static ssize_t store_timer_rate(struct cpufreq_interactive_tunables *tunables,
 	spin_unlock_irqrestore(&tunables->param_index_lock, flags_idx);
 #else
 	tunables->timer_rate = val_round;
-	tunables->prev_timer_rate = val_round;
 #endif
 	return count;
 }
