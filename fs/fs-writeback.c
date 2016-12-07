fd7fcc52483e94b4e7cb0382f573d5693c0d78db
WildfireDEV <wildfiredev@gmail.com>
2016-12-08 01:11:11 +1100

    sync: don't block the flusher thread waiting on IO
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -613,7 +613,7 @@ static long writeback_sb_inodes(struct super_block *sb,
 		.tagged_writepages	= work->tagged_writepages,
 		.for_kupdate		= work->for_kupdate,
 		.for_background		= work->for_background,
-		.for_sync		= work->for_sync,
+		.for_sync			= work->for_sync,
 		.range_cyclic		= work->range_cyclic,
 		.range_start		= 0,
 		.range_end		= LLONG_MAX,
@@ -1045,7 +1045,9 @@ void bdi_writeback_workfn(struct work_struct *work)
 	struct backing_dev_info *bdi = wb->bdi;
 	long pages_written;
 
-	set_worker_desc("flush-%s", dev_name(bdi->dev));
+	WARN_ON(!bdi->dev);
+	if (bdi->dev)
+		set_worker_desc("flush-%s", dev_name(bdi->dev));
 	current->flags |= PF_SWAPWRITE;
 
 	if (likely(!current_is_workqueue_rescuer() ||
